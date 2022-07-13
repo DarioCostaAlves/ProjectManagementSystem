40fd07ab4b964730e06ff40f635978c673ecad9d
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-07-13 17:21:36 +0100

    mutations: remove and update projects

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -113,9 +113,9 @@ const mutation = new GraphQLObjectType({
                 status: { type: new GraphQLEnumType({
                     name: 'ProjectStatus',
                         values: {
-                            'new': { value: 'Not Started' }, 
-                            'progress': { value: 'In Progress' }, 
-                            'completed': { value: 'Completed' },   
+                            new: { value: 'Not Started' }, 
+                            progress: { value: 'In Progress' }, 
+                            completed: { value: 'Completed' },   
                         }
                     }),
                     defaultValue: 'Not Started',
@@ -130,6 +130,43 @@ const mutation = new GraphQLObjectType({
                     clientId: args.clientId,
                 });
                 return project.save();
+            },
+        },
+        //Delete a Project
+        deleteProject: {
+            type: ProjectType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) },
+            },
+            resolve(parent, args){
+                return Project.findByIdAndRemove(args.id);
+            },            
+        },
+        //Update a Project
+        updateProject: {
+            type: ProjectType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                name: { type: GraphQLString },
+                description: { type: GraphQLString },
+                status: { type: new GraphQLEnumType({
+                    name: 'ProjectStatusUpdate',
+                        values: {
+                            new: { value: 'Not Started' }, 
+                            progress: { value: 'In Progress' }, 
+                            completed: { value: 'Completed' },   
+                        }
+                    }),
+                },
+            },
+            resolve(parent, args){
+                return Project.findByIdAndUpdate(args.id, {
+                    $set: {
+                        name: args.name,
+                        description: args.description,
+                        status: args.status,
+                    }
+                }, { new: true });
             },
         },
     }
