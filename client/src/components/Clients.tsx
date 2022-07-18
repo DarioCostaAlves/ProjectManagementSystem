6c9857c0bd8676609273b3c4fa490d15c73d3143
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-07-18 15:59:57 +0100

    spinner when loading get_clietns querie

--- client/src/components/Clients.tsx
+++ client/src/components/Clients.tsx
@@ -1,12 +1,13 @@
 import { gql, useQuery } from '@apollo/client';
 import ClientRow from './ClientRow';
+import Spinner from './Spinner';
 import {GET_CLIENTS} from '../queries/clientQueries';
 
 export default function Clients() {
   //Apollo and apollo providers is basically a state manager
   const { loading, error, data } = useQuery(GET_CLIENTS);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Spinner/>;
   if (error) return <p>Something went wrong</p>;
 
   return (
