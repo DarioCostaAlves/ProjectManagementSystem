6256921eb494ffc009d6652ddd8f8567d6383d39
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-07-19 17:27:18 +0100

    building AddClient modal

---
+++ client/src/components/AddClientModal.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+
+export default function AddClientModal() {
+  return (
+    <>        
+        <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addClientModal">
+            <div className="d-flex align-items-center">
+                <FaUser className="icon"/>
+                <div>
+                    Add Client
+                </div>
+            </div>
+            
+        </button>
+
+        <div className="modal fade" id="addClientModal" tabIndex="-1" aria-labelledby="addClientModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="addClientModalLabel">Add Client</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <form action="">
+                            <div className="mb-3">
+                                <label className="form-label">Name</label>
+                                <input type="text" className="form-control" id="name"/>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  );
+}
