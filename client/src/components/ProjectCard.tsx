cf273668743c45e4f83d1263fd98386c55cb6337
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-07-24 17:32:11 +0100

    show project status-

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -7,7 +7,12 @@ export default function ProjectCard({ project }) {
                     <h5 className="card-title">
                         {project.name}
                     </h5>
+
+                    <a className="btn btn-light" href="{`/projects/${project.id}`}">
+                        View
+                    </a>
                 </div>
+                <p className="small">Status : <strong>{project.status}</strong></p>
             </div>
         </div>
     </div>
